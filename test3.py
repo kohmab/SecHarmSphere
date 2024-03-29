091d21772aa1e2e1561aba7b03e7c4b90a8b79d0
kohmab <kohmab@gmail.com>
2024-03-29 16:12:34 +0300

    test for epsinf and laplacian in rhs

--- test3.py
+++ test3.py
@@ -6,20 +6,18 @@ from Problem import Problem
 from scipy.special import spherical_jn
 
 
-def rhsPhi(r, w):
-    return A1*G(k1, r)
+def rhs(r, w):
+    return G(k0, r)
 
 
-multipoleIndex = 0
+multipoleIndex = 2
 N = 1001
-epsD = 1000.
-epsInf = 1
-r0 = .04
-nu = 5
-w = 0.5
-k1 = 0.1
-A1 = 1.
-Q = 0
+epsD = 1
+epsInf = 10
+r0 = 0.1
+nu = 1
+w = 1.5
+k0 = 13.8461118773266
 
 
 def G(k, r):
@@ -30,7 +28,7 @@ def G(k, r):
 
 
 p: Problem = Problem(N, multipoleIndex, nu, w, r0,
-                     epsD, epsInf=epsInf, rhsrho=None, rhsphi=rhsPhi, Q0=Q)
+                     epsD, epsInf=epsInf, rhsphi=rhs)
 
 phi = p.getPhi()
 rho = p.getRho()
@@ -38,36 +36,33 @@ rho = p.getRho()
 r = p.getR()
 
 
-# theor sol
+eps = 1 - 1.0 / w / (w + 1j * nu)
+kpa = np.sqrt(w * (w + 1j * nu) - 1./epsInf) / r0
+epsD = epsD/epsInf
 n = multipoleIndex
-kp = np.sqrt(w * (w + 1j * nu) - 1./epsInf) / r0
+rho_teor = -(kpa**2) / k0**2 / (kpa**2 - k0**2) * (
+    epsD * (n + 1) * (1 + epsD * (n + 1) * G(k0, 1)*k0**2/4/np.pi)
+    + (n + epsD * (n + 1)) * (k0**2 * r0**2 -
+                              epsD * (n + 1) * G(k0, 1)*k0**2/4/np.pi)
+) / (
+    epsD * (n + 1) * (1 + epsD * (n + 1) * G(kpa, 1))
+    + (n + epsD * (n + 1)) * (kpa**2 * r0**2 - epsD * (n + 1) * G(kpa, 1))
+) * G(
+    kpa, r
+) + G(
+    k0, r
+)*k0**2/4/np.pi / (
+    kpa**2 - k0**2
+)
+# phi_teor = C1*r + 4 * np.pi / kpa ** 2 * rho_teor
 
-al = A1*k1**2/(kp**2-k1**2)/4./np.pi
-gm = 1./4/np.pi/epsInf/kp**2
-e0 = epsD/epsInf
-M = np.eye(2, dtype=complex)
-D = np.ones(2, dtype=complex)
-M[0, 0] = n
-M[0, 1] = gm+4*np.pi*r0**2
-M[1, 0] = n+e0*(n+1)
-M[1, 1] = gm*(1+e0*(n+1)*G(kp, 1))
-D[0] = -gm*al-4*np.pi*r0**2*al-r0**2*A1
-D[1] = -gm*al+e0*((2*n+1)*Q-(n+1)*gm*(al+A1/4/np.pi)*G(k1, 1))
-ca = np.linalg.solve(M, D)
-C = ca[0]
-A = ca[1]
-print(M@ca-D)
-
-
-rho_teor = A*G(kp, r)+al*G(k1, r)
-phi_teor = C*r**n + gm*(rho_teor+A1*G(k1, r)/4/np.pi)
 
 plt.figure(1)
 plt.title(r"$\varphi$")
 plt.plot(r, phi.real, "r")
 plt.plot(r, phi.imag, "b")
-plt.plot(r, phi_teor.real, "k:")
-plt.plot(r, phi_teor.imag, "k:")
+# plt.plot(r, phi_teor.real, "k:")
+# plt.plot(r, phi_teor.imag, "k:")
 plt.grid()
 
 plt.figure(2)
