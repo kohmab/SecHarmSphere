b4798420b97bfb34add608d288b50972ce175089
kohmab <kohmab@gmail.com>
2024-04-02 13:09:42 +0300

    refactor Oscillation

--- DipoleOscillation.py
+++ DipoleOscillation.py
@@ -4,60 +4,39 @@ from Oscillation import Oscillation
 
 
 class DipoleOscillation(Oscillation):
-    def __init__(self,
-                 N: int,
-                 nu: np.double, r0: np.double,
-                 epsD: np.double = 1.,
-                 epsInf: np.double = 1.) -> None:
-        self.__nu = nu
-        self.__r0 = r0
-        self.__epsD = epsD
-        self.__epsInf = epsInf
-        self.__r = np.linspace(0, 1, N)
+    def __init__(self, N: int, nu, r0, epsD=1, epsInf=1):
+        super().__init__(N, 1, nu, r0, epsD, epsInf)
 
-    def __radFunc(self, r):
-        return spherical_jn(1, self.__kp*r)/self.__kp/spherical_jn(1, self.__kp, True)
+    def __radFunc(self):
+        return spherical_jn(1, self.__kp*self.r)/self.__kp/spherical_jn(1, self.__kp, True)
 
     def __updateFreq(self, w):
-        self.__kp = np.sqrt(w*(w+1j*self.__nu) - 1. /
-                            self.__epsInf) / self.__r0
-        self.__eps = self.__epsInf - 1. / w/(w + 1j * self.__nu)
+        self.freq = w
+        if not self._isFreqChanged:
+            return
+        self.__kp = np.sqrt(w*(w+1j*self.nu) - 1. /
+                            self.epsInf) / self.r0
+        self.__eps = self.epsInf - 1. / w/(w + 1j * self.nu)
 
         v0 = jve(1.5, self.__kp)
         v1 = jve(2.5, self.__kp)
         self.__G = v0 / (v0 - v1*self.__kp)
 
-        denum = self.__eps + 2 * self.__epsD * \
-            (1 + (self.__eps - self.__epsInf)/self.__epsInf*self.__G)
-        self.__C = -3 * self.__epsD/denum
+        denum = self.__eps + 2 * self.epsD * \
+            (1 + (self.__eps - self.epsInf)/self.epsInf*self.__G)
+        self.__C = -3 * self.epsD/denum
+
+        self._isFreqChanged = False
 
     def getPhi(self, w):
         self.__updateFreq(w)
-        return self.__C*(self.__r - self.__radFunc(self.__r)/w/(w + 1j * self.__nu)/self.__epsInf)
+        return self.__C*(self.r - self.__radFunc()/w/(w + 1j * self.nu)/self.epsInf)
 
     def getRho(self, w):
         self.__updateFreq(w)
-        return -self.__C*self.__eps/(4.*np.pi*self.__r0**2 * self.__epsInf)*self.__radFunc(self.__r)
+        return -self.__C*self.__eps/(4.*np.pi*self.r0**2 * self.epsInf)*self.__radFunc()
 
     def getPsi(self, w):
-        phi = self.getPhi()
-        rho = self.getRho()
-        return phi + 4*np.pi*self.__r0**2 * rho
-
-    def getR(self):
-        return self.__r
-
-    def getMultipoleIndex(self):
-        pass
-
-    def getRhoExtMono(self):
-        pass
-
-    def getRhoExtQuadro(self):
-        pass
-
-    def getPhiExtMono(self):
-        pass
-
-    def getPhiExtQuadro(self):
-        pass
+        phi = self.getPhi(w)
+        rho = self.getRho(w)
+        return phi + 4*np.pi*self.r0**2 * rho
--- Oscillation.py
+++ Oscillation.py
@@ -1,7 +1,25 @@
 from abc import abstractclassmethod
+from abc import ABC
+import re
+import numpy as np
 
 
-class Oscillation():
+class Oscillation(ABC):
+
+    def __init__(self,
+                 N: int,
+                 m: int,
+                 nu: np.double, r0: np.double,
+                 epsD: np.double = 1.,
+                 epsInf: np.double = 1.) -> None:
+        self.__nu = nu
+        self.__multipoleNo = m
+        self.__r0 = r0
+        self.__epsD = epsD
+        self.__epsInf = epsInf
+        self.__r = np.linspace(0, 1, N)
+        self._isFreqChanged = True
+        self.__w = None
 
     @abstractclassmethod
     def getPhi(self, w):
@@ -12,13 +30,43 @@ class Oscillation():
         pass
 
     @abstractclassmethod
-    def getKsi(self, w):
+    def getPsi(self, w):
         pass
 
-    @abstractclassmethod
-    def getR(self):
-        pass
+    @property
+    def r(self):
+        return self.__r
 
-    @abstractclassmethod
-    def getMultipoleIndex(self):
-        pass
+    @property
+    def nu(self):
+        return self.__nu
+
+    @property
+    def r0(self):
+        return self.__r0
+
+    @property
+    def epsD(self):
+        return self.__epsD
+
+    @property
+    def epsInf(self):
+        return self.__epsInf
+
+    @property
+    def multipoleN0(self):
+        return self.__multipoleNo
+
+    @property
+    def N(self):
+        return self.__N
+
+    @property
+    def freq(self):
+        return self.__w
+
+    @freq.setter
+    def freq(self, w):
+        if self.__w != w:
+            self._isFreqChanged = True
+        self.__w = w
