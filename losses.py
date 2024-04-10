899fea756bc9cec492fa9a7bc09ee6766b8bcfb0
kohmab <kohmab@gmail.com>
2024-04-10 15:57:16 +0300

    fixed typos

--- losses.py
+++ losses.py
@@ -14,31 +14,32 @@ def getLossesAtOneFreq(osc: Oscillation, w: float):
     rhoConj = osc.getRho(w).conj()
     rsq = np.square(osc.r)
     # @TODO calculate with Integrals
-    integralOverR = np.trapz(osc.r, psi*rhoConj*rsq)
+    integralOverR = np.trapz(psi*rhoConj*rsq, osc.r)
     integralOverTheta = 2./(osc.multipoleN0*2 + 1)
     integralOverPhi = 2*np.pi
-    coef = -osc.nu/2*(osc.freq/(osc.freq + 1j*osc.nu))
+    coef = osc.nu*osc.freq/(osc.freq + 1j*osc.nu)/2
     return np.real(coef*integralOverPhi*integralOverTheta*integralOverR)
 
 
 def getLosses(osc: Oscillation, w: np.ndarray) -> np.ndarray:
     result = np.zeros_like(w)
     print(f"Calculating losses for multipole #{osc.multipoleN0}...")
-    for i, freq in tqdm(enumerate(w)):
+    total = w.size
+    for i, freq in tqdm(enumerate(w), total=total):
         result[i] = getLossesAtOneFreq(osc, freq)
     return result
 
 
 if __name__ == "__main__":
-    nu = 0.01
+    nu = 0.05
     r0 = 0.1
     epsD = 1.
     epsInf = 1.
-    beta = 0.01
-    N = 1001
+    beta = 0.05
+    N = 2001
 
     wmin = 0.2
-    wmax = 1.1
+    wmax = 2
     Nw = 1000
     w = np.linspace(wmin, wmax, Nw)
 
@@ -52,7 +53,7 @@ if __name__ == "__main__":
         losses.append(getLosses(osc, w))
 
     plt.figure(1)
-    for i in range(3):
-        plt.plot(w, losses[i])
+    for l in losses:
+        plt.plot(w, l)
     plt.grid()
     plt.show()
